cf1247b7fba643814dcf9cb3c4a5115d5becc4cd
Angel Jimenez <jimenezprieto0508@gmail.com>
2021-02-23 16:02:19 -0500

    Agregar archivos de proyecto.

---
+++ WebApiProject/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProject
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
